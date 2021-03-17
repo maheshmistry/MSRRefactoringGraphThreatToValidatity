9477dac5fd4ad8782e86ad05f131f6cb3d28fafb
maheshmistry <maheshmistry879@gmail.com>
2021-03-17 22:30:54 +0100

    C++ and Javascript language added to python project

--- process/refactoring-graph-scripts/scripts/python/graph_generator.py
+++ process/refactoring-graph-scripts/scripts/python/graph_generator.py
@@ -4,12 +4,20 @@ from modules import datasetconfig
 from modules import util
 import sys
 
-def get_subgraphs_properties():
-  projects = datasetconfig.get_java_projects()
+def get_subgraphs_properties(lang='java'):
+
+  langs={
+        'java':datasetconfig.get_java_projects,
+        'cpp':datasetconfig.get_cpp_projects,
+        'js':datasetconfig.get_js_projects
+        }
+  lang_project = langs.get(lang,"Invalid Language")
+  projects = lang_project()
+
   head = 'id;project;vertices;edges;age_days;developers;distinct_operations;commits'
   lines = []
   for project_name in projects:
-    subgraphs = util.read_json('../../../../data/dataset/saner-2020/graphs/overtime_subgraphs_{}.json'.format(util.get_name_project_formated(project_name)))
+    subgraphs = util.read_json('../../../../data/dataset/graphs/overtime_subgraphs_{}.json'.format(util.get_name_project_formated(project_name)))
     for subgraph in subgraphs:
       id = subgraph.get('id_intra_project')
       vertices = len(prop.get_vertices(subgraph))
@@ -20,22 +28,19 @@ def get_subgraphs_properties():
       commits = len(prop.get_commits(subgraph))
       line = '{};{};{};{};{};{};{};{}'.format(id,project_name,vertices,edges,age_days,developers,distinct_operations,commits)
       lines.append(line)
-  util.write_csv('../../../../data/dataset/saner-2020/subgraphs_properties.csv', head, lines)
+  util.write_csv('../../../../data/dataset/{}_subgraphs_properties.csv'.format(lang), head, lines)
   pass
 
 def graph_generator(option):
   options = {
-    0: sys.exit(),
     1: generator.find_disconnected_subgraphs,
     2: generator.create_views,
     3: get_subgraphs_properties
-    
   }
   options[option]()
   return
 
 def menu():
-  print('\n\n0 - Exit')
   print('1 - Generate subgraphs')
   print('2 - Generate views')
   print('3 - See subgraphs properties (size, age, number of distinct operations, etc)')
@@ -49,4 +54,4 @@ def run():
     graph_generator(option)
   return
 
-run()
+#run()
--- process/refactoring-graph-scripts/scripts/python/modules/datasetconfig.py
+++ process/refactoring-graph-scripts/scripts/python/modules/datasetconfig.py
@@ -1,4 +1,5 @@
 from modules import util
+import pandas as pd
 
 dictionary = {}
 dictionary["RENAME"] = 101
@@ -14,7 +15,21 @@ dictionary["INLINE"] = 108
 
 config = {}
 
-config['java_projects'] = {'elastic/elasticsearch', 'ReactiveX/RxJava', 'square/okhttp', 'square/retrofit', 'spring-projects/spring-framework', 'apache/dubbo', 'PhilJay/MPAndroidChart', 'bumptech/glide', 'airbnb/lottie-android', 'facebook/fresco'}
+#config['java_projects'] = {'elastic/elasticsearch', 'ReactiveX/RxJava', 'square/okhttp', 'square/retrofit', 'spring-projects/spring-framework', 'apache/dubbo', 'PhilJay/MPAndroidChart', 'bumptech/glide', 'airbnb/lottie-android', 'facebook/fresco'}
+java_csv_file = '../../../../data/dataset/java_git_projects.csv'
+
+java_df = pd.read_csv(java_csv_file, usecols=["project"], delimiter=';')
+config['java_projects'] = set(java_df['project'].unique())
+
+cpp_csv_file = '../../../../data/dataset/cpp_git_projects.csv'
+
+cpp_df = pd.read_csv(cpp_csv_file, usecols=["project"], delimiter=';')
+config['cpp_projects'] = set(cpp_df['project'].unique())
+
+js_csv_file = '../../../../data/dataset/js_git_projects.csv'
+
+js_df = pd.read_csv(js_csv_file, usecols=["project"], delimiter=';')
+config['js_projects'] = set(js_df['project'].unique())
 
 config['refdiff_fields'] = ['name_project', 'sha1', 'entity_before_full_name', 'entity_before_simple_name', 'entity_before_location', 'entity_after_full_name', 'entity_after_simple_name', 'entity_after_location', 'refactoring_level', 'refactoring_name']
 #'url', 'entity_before_is_internal_level', 'entity_after_is_internal_level', 'entity_before_parameters', 'entity_after_parameters', 'entity_before_line', 'entity_after_line']
@@ -33,7 +48,16 @@ def get_dictionary():
   return dictionary
 
 def get_java_projects():
+  print('java')
   return config.get('java_projects')
+
+def get_cpp_projects():
+  print('cpp')
+  return config.get('cpp_projects')
+
+def get_js_projects():
+  print('js')
+  return config.get('js_projects')
 
 def get_commit_fields():
   return config.get('commit_fields')
